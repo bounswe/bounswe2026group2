b7ceaff4e9991d6caee51298f3f5a80128337938
akifemrebicen <akifebicen@gmail.com>
2026-03-12 14:04:42 +0300

    Cat button

--- handlers.js
+++ handlers.js
@@ -28,3 +28,8 @@ function onButton4Click() {
 function onButton5Click() {
   console.log("Button 5 clicked -- implement me!");
 }
+
+
+function onButton8Click() {
+  window.location.href = "cat.html"
+}
